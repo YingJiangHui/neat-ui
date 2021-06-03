3fa199fa3c2783a7aaaeabaa237b2a8000476820
Ying <473380917@qq.com>
2021-06-03 18:07:53 +0800

    withDefaults 将defaultProps加入组件

---
+++ src/utils/withDefaults.ts
@@ -0,0 +1,11 @@
+import React from 'react';
+const withDefaults = <P, DP>(
+  component: React.ComponentType<P>,
+  defaultProps: DP,
+) => {
+  type Props = Partial<P> & Omit<P, keyof DP>; // 可选的所有类型 & 在所有类型中排除默认的 效果：可以不填有默认的值，必须填没有默认的值
+  component.defaultProps = defaultProps;
+  return component as React.ComponentType<Props>;
+};
+
+export default withDefaults;
